88686c8de34a4c7d8df5898193c5958e3c0356da
Tamilanproject <107706438+Tamilanproject@users.noreply.github.com>
2022-06-17 22:17:07 +0530

    Add files via upload

---
+++ LobbyActivity.java
@@ -0,0 +1,14 @@
+package com.tamilanprojects.officials.games.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LobbyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lobby);
+    }
+}
